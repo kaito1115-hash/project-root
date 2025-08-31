6d4c92ebe2cba33fad44954c3f9507b39f6916b4
Kaito <replika1115@gmail.com>
2025-08-31 13:53:39 +0900

    Initial commit

---
+++ src/app/func.php
@@ -0,0 +1,17 @@
+<?php 
+$age = 25;
+echo "Your age is " . $age;
+ ?>
+
+ <?php 
+ $fruits = ["Apple", "Banana", "Cherry"];
+ echo $fruits[0];
+  ?>
+
+  <?php 
+  $person = [
+    "name" => "Jhon",
+    "age"=> "30",
+    "city"=> "New York",
+  ];
+  echo $person['age'] ?>
